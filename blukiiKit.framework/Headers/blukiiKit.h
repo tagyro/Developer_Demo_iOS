8f7b67b9075acba9a7e2c44306f9e4a85ba45ce9
lennart683 <lennart.fleig@schneiderpen.de>
2016-05-31 09:54:22 +0200

    framework added

---
+++ blukiiKit.framework/Headers/blukiiKit.h
@@ -0,0 +1,19 @@
+//
+//  BlukiiKit.h
+//  BlukiiKit
+//
+//  Created by Thorsten Kober on 16.09.14.
+//  Copyright (c) 2014 SemVox GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BlukiiKit.
+FOUNDATION_EXPORT double BlukiiKitVersionNumber;
+
+//! Project version string for BlukiiKit.
+FOUNDATION_EXPORT const unsigned char BlukiiKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlukiiKit/PublicHeader.h>
+
+
